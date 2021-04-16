51a0f8636b18b8977d6c35d4293f1586e1826a96
kdaquila <kfd182@gmail.com>
2021-04-15 20:23:03 -0500

    Removed unused dependency.

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ require('dotenv').config()
 
 const app = require('./app');
 const http = require('http');
-const killPort = require('kill-port');
 
 // Get port from environment and store in Express.
 const port = normalizePort(process.env.PORT || process.env.APP_PORT || 3000);
